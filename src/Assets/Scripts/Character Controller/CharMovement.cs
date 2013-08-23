3ec28725e01278c4da654ab8a4d7c2aae095176a
azure-dragon <shazadali09@hotmail.co.uk>
2013-08-23 20:08:53 +0100

    first proper commit

---
+++ src/Assets/Scripts/Character Controller/CharMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+//[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
+public class CharMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+		
+	public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+	public Transform bullitPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		var controller = GetComponent<CharacterController>();
+	
+		//rotate around y - axis
+		//note: x and z we are not doing anything with...
+		transform.Rotate(0,Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		
+		var forward = transform.TransformDirection(Vector3.forward);
+		var curSpeed = speed * Input.GetAxis("Vertical");
+		controller.SimpleMove(forward * curSpeed);
+	
+	    if(Input.GetButtonDown("Jump"))
+		{
+			
+		   var bullit = Instantiate(bullitPrefab,
+				GameObject.Find("spawnpoint").transform.position,
+				Quaternion.identity) as Transform;
+		
+			bullit.rigidbody.AddForce(transform.forward * 2000);
+		}
+	}
+}
